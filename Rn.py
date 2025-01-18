cb83d76754461c978e222e6481203250d66d24db
Finn Sherry <fms2000@live.com>
2025-01-18 20:49:08 +0100

    move unnecessary private methods into group classes.

--- Rn.py
+++ Rn.py
@@ -11,37 +11,25 @@ class Rn():
         self.dim = n
     
     def L(self, g_1, g_2):
-        return _L(g_1, g_2)
+        """
+        Left multiplication of `g_2` by `g_1`, i.e. `g_1 + g_2`.
+        """
+        return g_1 + g_2
     
     def L_inv(self, g_1, g_2):
-        return _L_inv(g_1, g_2)
+        """
+        Left multiplication of `g_2` by `g_1^-1`, i.e. `g_2 - g_1`.
+        """
+        return g_2 - g_1
     
     def log(self, g):
-        return _log(g)
+        """
+        Lie group logarithm of `g`, i.e. `A` in Lie algebra such that `exp(A) = g`.
+        """
+        return g.clone()
     
     def exp(self, A):
-        return _exp(A)
-
-def _L(g_1, g_2):
-    """
-    Left multiplication of `g_2` by `g_1`, i.e. `g_1 + g_2`.
-    """
-    return g_1 + g_2
-
-def _L_inv(g_1, g_2):
-    """
-    Left multiplication of `g_2` by `g_1^-1`, i.e. `g_2 - g_1`.
-    """
-    return g_2 - g_1
-
-def _log(g):
-    """
-    Lie group logarithm of `g`, i.e. `A` in Lie algebra such that `exp(A) = g`.
-    """
-    return g.clone()
-
-def _exp(A):
-    """
-    Lie group exponential of `A`, i.e. `g` in Lie group such that `exp(A) = g`.
-    """
-    return A.clone()
+        """
+        Lie group exponential of `A`, i.e. `g` in Lie group such that `exp(A) = g`.
+        """
+        return A.clone()
